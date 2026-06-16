3797b4bef989f549023413f16d4a057fc43e1094
tmchao7 <tianmc@mails.neu.edu.cn>
2026-06-16 16:35:21 +0800

    fix: save button no-op + boss inputs too narrow
    - optSaveButton_ connect was removed during UI restructure; re-added
    - bossHealthEdit_ and skillsEdit_ setMinimumWidth(200) to avoid horizontal scroll
    - skillsEdit_ placeholder text reads better now

--- src/source/mainwindow.cpp
+++ src/source/mainwindow.cpp
@@ -210,6 +210,7 @@ void MainWindow::buildUi() {
     optSaveButton_->setObjectName(QStringLiteral("secondaryButton"));
     optSaveButton_->setEnabled(false);
     generationOuter->addWidget(optSaveButton_);
+    connect(optSaveButton_, &QPushButton::clicked, this, &MainWindow::saveOptimizedMaze);
 
     panelLayout->addWidget(generationGroup);
 
@@ -295,11 +296,13 @@ void MainWindow::buildUi() {
     bossHealthEdit_->setObjectName(QStringLiteral("inputControl"));
     bossHealthEdit_->setPlaceholderText(QStringLiteral("如 35,45,60"));
     bossHealthEdit_->setToolTip(QStringLiteral("各 BOSS 血量，逗号分隔"));
+    bossHealthEdit_->setMinimumWidth(200);
     skillsEdit_ = new QLineEdit(
         QStringLiteral("强力攻击:8:4;普通攻击:2:0;连击:4:2;重击:6:3"));
     skillsEdit_->setObjectName(QStringLiteral("inputControl"));
     skillsEdit_->setPlaceholderText(QStringLiteral("名称:伤害:冷却;..."));
     skillsEdit_->setToolTip(QStringLiteral("如 普通攻击:5:0;重击:10:2"));
+    skillsEdit_->setMinimumWidth(200);
     bossForm->addRow(QStringLiteral("血量"), bossHealthEdit_);
     bossForm->addRow(QStringLiteral("技能"), skillsEdit_);
     bossLayout->addLayout(bossForm);
